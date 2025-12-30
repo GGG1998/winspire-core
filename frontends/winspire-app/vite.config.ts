7253478e206b5880da3dd616fb47ae4d5a280b5d
Gabriel <gabriel.domanowski@gowinspire.com>
2025-12-30 12:14:38 +0100

    chore: update Vite configuration to standardize allowed hosts for server and preview
    - Refined the Vite config to ensure consistent allowed hosts for both server and preview environments, enhancing security and clarity in the configuration.

--- frontends/winspire-app/vite.config.ts
+++ frontends/winspire-app/vite.config.ts
@@ -4,7 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'localhost',
+      'winspire-dev-s63lr.ondigitalocean.app',
+      'dev-api.gowinspire.com',
+    ],
+  },
   preview: {
-    allowedHosts: ['winspire-dev-s63lr.ondigitalocean.app, localhost', 'dev-api.gowinspire.com'],
+    allowedHosts: [
+      'localhost',
+      'winspire-dev-s63lr.ondigitalocean.app',
+      'dev-api.gowinspire.com',
+    ],
   },
 })
